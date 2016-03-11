732a0182cf25a44581d0c598d1751f07c5abb4c1
Frédéric Scheubel <fredsifu@hotmail.com>
2016-03-11 18:49:07 +0000

    Integrated a categorization feature and some TestDD

---
+++ app/models/category.rb
@@ -0,0 +1,6 @@
+class Category < ActiveRecord::Base
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+  
+  validates :name, presence: true, uniqueness: true, length: { in: 3..25 }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,6 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   #post 'users', to: 'users#create'
   resources :users, except: [:new]
+  
+  resources :categories, except: [:destroy]
 end
